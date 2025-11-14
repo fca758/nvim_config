81e191a06a77fbb8de535c286101a3b03da67199
fca758 <fca758@inlumine.ual.es>
2025-11-14 09:45:24 +0100

    atajos de movimiento en modo del

--- init.lua
+++ init.lua
@@ -194,14 +194,16 @@ for k, v in pairs(directions) do
 end
 
 -- Ponemos las direcciones de nvim una más a la izquierda (h j k l) -> (j k l ñ)
-vim.keymap.set('n', 'j', 'h')
-vim.keymap.set('n', 'k', 'j')
-vim.keymap.set('n', 'l', 'k')
-vim.keymap.set('n', 'ñ', 'l')
-vim.keymap.set('v', 'j', 'h')
-vim.keymap.set('v', 'k', 'j')
-vim.keymap.set('v', 'l', 'k')
-vim.keymap.set('v', 'ñ', 'l')
+vim.keymap.set({"n","v","o"},"h","<nop>",{noremap=true,silent=true})
+vim.keymap.set({"n","v","o"},"j","<nop>",{noremap=true,silent=true})
+vim.keymap.set({"n","v","o"},"k","<nop>",{noremap=true,silent=true})
+vim.keymap.set({"n","v","o"},"l","<nop>",{noremap=true,silent=true})
+vim.keymap.set({"n","v","o"},"j","h",{noremap=true,silent=true})
+vim.keymap.set({"n","v","o"},"k","j",{noremap=true,silent=true})
+vim.keymap.set({"n","v","o"},"l","k",{noremap=true,silent=true})
+vim.keymap.set({"n","v","o"},"ñ","l",{noremap=true,silent=true})
+
+
 
 
 
