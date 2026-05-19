c807952d51fc6cf5b1fc6dca9335d2e00c4c6aed
fca758 <fca758@inlumine.ual.es>
2026-05-19 17:38:09 +0200

    fondo trasnparente (se cambia ez)

--- lua/colors.lua
+++ lua/colors.lua
@@ -6,3 +6,11 @@ else
     vim.cmd("colorscheme onedark")
 end
 
+-- Fondo transparente
+
+vim.cmd("highlight Normal ctermbg=NONE guibg=NONE")
+vim.cmd("highlight NonText ctermbg=NONE guibg=NONE")
+vim.cmd("highlight NormalFloat ctermbg=NONE guibg=NONE")
+vim.api.nvim_set_hl(0, "LineNr", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE" })
+
